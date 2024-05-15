514c225c096959ce1afae9af30a0df6a39bf81a4
Iusheva Evgeniia <evgeniia.iusheva@gmail.com>
2024-05-15 13:36:09 +0200

    folder Ãpages is moved to src-block

---
+++ src/main/java/com/pool/pagesSE/BasePage.java
@@ -0,0 +1,78 @@
+package com.pool.pagesSE;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    WebDriver driver;
+    JavascriptExecutor js;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        js =(JavascriptExecutor) driver;
+    }
+
+    public void click(WebElement element){
+        element.click();
+    }
+
+    public void type(WebElement element, String text){
+        if(text != null){
+            click(element);
+            element.clear();
+            element.sendKeys(text);
+        }
+
+    }
+
+    public void clickJS(WebElement element, int x, int y){
+        js.executeScript("window.scrollBy(" + x + "," + y +")");
+        click(element);
+    }
+
+    public  boolean shouldHaveText(WebElement element, String text, int time){
+        return new WebDriverWait(driver, Duration.ofSeconds(time))
+                .until(ExpectedConditions.textToBePresentInElement(element, text));
+    }
+
+
+
+    public  void  pause(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void verifyLinks(String linkUrl) {
+        try {
+
+            URL url = new URL(linkUrl);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(5000);
+            httpURLConnection.connect();
+
+            if(httpURLConnection.getResponseCode()>=400){
+                System.out.println(linkUrl + " - " + httpURLConnection.getResponseMessage() + " is a broken link");
+            } else {
+                System.out.println(linkUrl + " - "+ httpURLConnection.getResponseMessage());
+            }
+        } catch (Exception e){
+            System.out.println(linkUrl+ " - " + e.getMessage() + " ERROR occurred");
+        }
+
+    }
+
+
+}
