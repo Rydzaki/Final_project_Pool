6432f2046d2f1431eee8ce836f531396a0ca2843
Iusheva Evgeniia <evgeniia.iusheva@gmail.com>
2024-05-21 11:29:31 +0200

    orders tests

--- src/test/java/com/pool/testsRA/orders/GetOrderWithIdTests.java
+++ src/test/java/com/pool/testsRA/orders/GetOrderWithIdTests.java
@@ -1,39 +1,35 @@
 package com.pool.testsRA.orders;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.pool.dto.orders.NewOrdersDto;
 import com.pool.dto.orders.OrderDto;
-
+import com.pool.dto.product.ProductDto;
 import com.pool.testsRA.TestBase;
 import com.pool.testsRA.ZonedDateTimeAdapter;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import org.testng.annotations.Test;
 
-
 import java.time.ZonedDateTime;
-
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
+public class GetOrderWithIdTests extends TestBase {
 
-public class CreateNewOrdersTests extends TestBase {
-
+    private Integer ordersId = 1;
 
     @Test
-    public void createNewOrderTests() {
-
-        OrderDto newOrder = OrderDto.builder()
-                .userId(3)
-                .summa(25.77)
-                .itemsCount(1)
-                .date("2024-05-20T22:27:31.444Z")
+            public void testGetOrderWithId() {
+        NewOrdersDto newOrder = NewOrdersDto.builder()
+                .userId(1)
+                .summa(20)
+                .itemsCount(3)
+                .date("2024-05-19T19:30:18.591Z")
                 .build();
 
-        // Создаем объект Gson с зарегистрированным адаптером ZonedDateTime
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(ZonedDateTime.class, new ZonedDateTimeAdapter())
                 .setPrettyPrinting()
@@ -41,17 +37,14 @@ public class CreateNewOrdersTests extends TestBase {
 
         // Преобразуем объект OrdersDto в JSON строку
         String jsonRequest = gson.toJson(newOrder);
-
-        // Отправляем POST запрос для создания нового заказа
         OrderDto responseOrder = given()
                 .cookie(new Cookie.Builder(SESSION_ID, getCookiesForLogin().get(SESSION_ID).getValue()).build())
                 .contentType(ContentType.JSON)
-                .body(jsonRequest)
                 .when()
-                .post("/orders")
+                .get("/orders/" + ordersId)
                 .then()
                 .assertThat()
-                .statusCode(201)
+                .statusCode(200)
                 .body("id", notNullValue())
                 .body("userId", equalTo(newOrder.getUserId()))
                 .body("summa", equalTo((float) newOrder.getSumma()))
@@ -60,7 +53,8 @@ public class CreateNewOrdersTests extends TestBase {
 
         // Печатаем JSON ответа
         printJson(responseOrder);
+
+
+
     }
-
-
 }
