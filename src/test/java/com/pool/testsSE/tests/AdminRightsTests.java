c923a33d54c4d6ce24311059eede8769c051cfca
Iusheva Evgeniia <evgeniia.iusheva@gmail.com>
2024-05-30 18:38:55 +0200

    korrektur

--- src/test/java/com/pool/testsSE/tests/AdminRightsTests.java
+++ src/test/java/com/pool/testsSE/tests/AdminRightsTests.java
@@ -2,6 +2,7 @@ package com.pool.testsSE.tests;
 
 import com.pool.pagesSE.AdminPage;
 import com.pool.pagesSE.HomePage;
+import com.pool.pagesSE.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,7 +10,10 @@ import org.testng.annotations.Test;
 public class AdminRightsTests extends TestBaseSE{
     @BeforeMethod
     public void precondition() {
+
         new HomePage(driver).selectEnterBtn();
+        new LoginPage(driver).loginData("ushakov_test@mail.com","Pass12345!");
+        new LoginPage(driver).myProfilePage();
     }
 
     @Test
